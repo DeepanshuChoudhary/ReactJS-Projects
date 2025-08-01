c76e97f37ede97fa9fd20fffc6d001080b17f79a
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-08-01 23:56:36 +0530

    Currency Convertor have swap bug

---
+++ Project-03 CurrencyConvertor/src/Hooks/useCurrencyInfo.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState} from "react";
+
+const useCurrencyInfo = (currency) => {
+
+    const [data, setData] = useState({})
+
+
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json`)
+            .then((response) => response.json())
+            .then((response) => setData(response[currency]))
+        // console.log(data)
+                .catch((error) => console.log('There is some error in fetching' + error))
+}, [currency])
+
+    // console.log(data);
+    return data
+
+}
+
+export default useCurrencyInfo
