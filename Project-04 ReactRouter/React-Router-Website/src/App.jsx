1a24aad6ad49165452c6a4494458ff1a2aeda346
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-08-03 08:52:26 +0530

    Route website

---
+++ Project-04 ReactRouter/React-Router-Website/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from './Components/Header/Header'
+import Home from "./Components/Home/Home";
+import Footer from "./Components/Footer/Footer";
+
+
+const App = () => {
+
+  // const [count, setCount] = useState()
+
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ Project-04 ReactRouter/React-Router-Website/src/main.jsx
@@ -0,0 +1,50 @@
+import React, { StrictMode } from "react";
+import ReactDOM, { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css';
+import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Components/Home/Home";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import User from "./User/User"
+import Github from "./Github/Github"
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements (
+    <Route path="/" element={<Layout/>}>
+      <Route path='' element={<Home/>} />
+      <Route path='about' element={<About/>} />
+      <Route path='contact' element={<Contact/>} />
+      <Route path='user/:userid' element={<User />} />
+      <Route path='Github' element={<Github />} />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render (
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
