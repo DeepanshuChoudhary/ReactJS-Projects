0d9ee9d137c5707f0905f7cd84b9a024ef91abe2
Deepanshu Choudhary <deepengineeryt@gmail.com>
2025-08-14 01:03:14 +0530

    Creating Tasks T:3:20:00

--- Project-07 Employee Management System/Employee-Management-System/src/components/other/AllTask.jsx
+++ Project-07 Employee Management System/Employee-Management-System/src/components/other/AllTask.jsx
@@ -21,81 +21,24 @@ const AllTask = () => {
                 </div>
 
                 <div id='tasklist' className='h-[80%] overflow-auto'>
-                    {authData.employees.map((e) => {
+                    {authData.employees.map((e, idx) => {
 
-                        return <div className='bg-blue-600 py-2 px-4 rounded-lg flex justify-between mb-3' >
-                            <h5 className='w-1/5 text-lg font-medium'>{e.firstname}</h5>
-                            <h5 className='w-1/5 text-lg font-medium text-blue-600'>{e.taskCount.new_task}</h5>
-                            <h5 className='w-1/5 text-lg font-medium text-purple-600'>{e.taskCount.active}</h5>
-                            <h5 className='w-1/5 text-lg font-medium text-green-600'>{e.taskCount.completed}</h5>
-                            <h5 className='w-1/5 text-lg font-medium text-red-600'>{e.taskCount.failed}</h5>
-                        </div>
+                        return (
+                            <div key={idx} className='bg-blue-600 py-2 px-4 rounded-lg flex justify-between mb-3' >
+                                <h5 className='w-1/5 text-lg font-medium'>{e.firstname}</h5>
+                                <h5 className='w-1/5 text-lg font-medium text-blue-600'>{e.taskCount.new_task}</h5>
+                                <h5 className='w-1/5 text-lg font-medium text-purple-600'>{e.taskCount.active}</h5>
+                                <h5 className='w-1/5 text-lg font-medium text-green-600'>{e.taskCount.completed}</h5>
+                                <h5 className='w-1/5 text-lg font-medium text-red-600'>{e.taskCount.failed}</h5>
+                            </div>
+                        )
 
                     })}
 
                 </div>
 
-                {/* <div className='bg-green-700 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Aryan Singh</h2>
-                    <h3>Password Generator</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-blue-600 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Sarthak Sharma</h2>
-                    <h3>Real Time Currency Converter</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-purple-400 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Aman Kasana</h2>
-                    <h3>React Route Website</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-pink-400 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Aakash Kasana</h2>
-                    <h3>ReactJS</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-orange-400 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Nishant Nagar</h2>
-                    <h3>Website Project</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-amber-900 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Ajay Nagar</h2>
-                    <h3>Website Project</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-blue-800 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Anshu Nagar</h2>
-                    <h3>Website Project</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-cyan-500 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Ankit Sharma</h2>
-                    <h3>Website Project</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-emerald-900 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Amit Kamal</h2>
-                    <h3>Website Project</h3>
-                    <h5>Status</h5>
-                </div>
-
-                <div className='bg-indigo-900 py-2 px-4 rounded-lg flex justify-between mb-3'>
-                    <h2>Amit Raj</h2>
-                    <h3>Website Project</h3>
-                    <h5>Status</h5>
-                </div> */}
             </div>
-        </div >
+        </div>
 
     )
 }
--- Project-07 Employee Management System/Employee-Management-System/src/components/other/CreateTask.jsx
+++ Project-07 Employee Management System/Employee-Management-System/src/components/other/CreateTask.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateTask = () => {
+
+    const [taskTitle, setTaskTitle] = useState('');
+    const [taskDescription, setTaskDescription] = useState('');
+    const [taskDate, setTaskDate] = useState('');
+    const [assignTo, setAssignTo] = useState('');
+    const [category, setCategory] = useState('');
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        setTaskTitle('');
+        setTaskDescription('');
+        setTaskDate('');
+        setAssignTo('');
+        setCategory('');
+    }
+
     return (
         <div className='p-5 bg-[#242424] mt-7 rounded-lg'>
-                <form className='flex flex-wrap w-full items-start justify-between'>
+                <form onSubmit={(e) => {
+                    submitHandler(e)
+                }} className='flex flex-wrap w-full items-start justify-between'>
+                    
                     <div className='w-1/2'>
                         <div>
                             <h3>Task Title</h3>
                             <input type='text' placeholder='Add the task'
+                                value={taskTitle}
+                                onChange={((e)=> {
+                                    setTaskTitle(e.target.value)
+                                })}
                                 className='text-sm py-1 w-4/5 rounded-lg outline-none bg-[#1c1c1c] border-gray-400 mb-4 px-2'
                             />
                         </div>
@@ -15,6 +39,11 @@ const CreateTask = () => {
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
                             <input type='date'
+                                value={taskDate}
+                                // value={taskDate || ''}
+                                onChange={((e) => {
+                                    setTaskDate(e.target.value)
+                                })}
                                 className='text-sm py-1 w-4/5 rounded-lg outline-none bg-[#1c1c1c] border-gray-400 mb-4 px-2'
                             />
                         </div>
@@ -22,6 +51,10 @@ const CreateTask = () => {
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'>Assign to</h3>
                             <input type='text' placeholder='Employee Name'
+                                value={assignTo}
+                                onChange={((e) => {
+                                    setAssignTo(e.target.value)
+                                })}
                                 className='text-sm py-1 w-4/5 rounded-lg outline-none bg-[#1c1c1c] border-gray-400 mb-4 px-2'
                             />
                         </div>
@@ -29,6 +62,10 @@ const CreateTask = () => {
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
                             <input type='text' placeholder='JavaScript, ReactJS, etc...'
+                                value={category}
+                                onChange={((e) => {
+                                    setCategory(e.target.value)
+                                })}
                                 className='text-sm py-1 w-4/5 rounded-lg outline-none bg-[#1c1c1c] border-gray-400 mb-4 px-2'
                             />
                         </div>
@@ -36,7 +73,12 @@ const CreateTask = () => {
 
                     <div className='w-2/5 flex flex-col items-start'>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
-                        <textarea className='w-full h-44 text-sm py-2 px-4 rounded-lg outline-none bg-[#1c1c1c] border-gray-400' name="" id=""></textarea>
+                        <textarea
+                            value={taskDescription}
+                            onChange={((e) => {
+                                setTaskDescription(e.target.value)
+                            })}
+                            className='w-full h-44 text-sm py-2 px-4 rounded-lg outline-none bg-[#1c1c1c] border-gray-400' name="" id=""></textarea>
                         <button className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full'>Create Task</button>
                     </div>
 
